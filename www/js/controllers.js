2ee0490aefce0fff50f4ad609814c7d4ea4cfab7
Arely Valdes <arely_0793@hotmail.es>
2015-09-21 21:57:09 +0000

    Se modifico form

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -5,18 +5,25 @@ angular.module('starter.controllers', [])
     var alertPopup = $ionicPopup.alert({
         title: 'Agenda' ,
         template: 'Datos guardados'
-    });
+        
+         });
+    
     }
+    
+
+
+$scope.guardar = function(persona){
+    console.log("Nombre: "+persona.nombre);
+    console.log("Apellido: "+persona.apellido);
+    console.log("Telefono: "+persona.telefono);
+    console.log("Email: "+persona.email);
+
+}
 })
 
+
 .controller('ChatsCtrl', function($scope, Chats) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+ 
 
   $scope.chats = Chats.all();
   $scope.remove = function(chat) {
